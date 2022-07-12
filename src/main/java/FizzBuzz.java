759812c05e0c302b1f54bc5fc856942322a106e2
Abdella <abdella.shukur2@gmail.com>
2022-07-12 12:36:30 -0400

    All tests are currently passing

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,8 @@
 public class FizzBuzz {
-  public String fizzBuzzString(String str) {
-      if (str.startsWith("f"))  return "Fizz";
-      if (str.endsWith("b")) return "Buzz";
+  public static String fizzBuzzString(String str) {
+      if (str == "f") return "Fizz";
+      
+      if (str == "b") return "Buzz";
 
       if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
 
