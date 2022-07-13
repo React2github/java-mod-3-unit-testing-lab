ddd71363ff2d9f783d6e7b4c0bd8f4c6257caf42
Abdella <abdella.shukur2@gmail.com>
2022-07-12 20:58:29 -0400

    added test for null string and fixed logic

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,8 @@
 public class FizzBuzz {
   public static String fizzBuzzString(String str) {
+      if (str == "") return null;
       if (str == "f") return "Fizz";
-      
       if (str == "b") return "Buzz";
-
       if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
 
       return str;
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,20 +3,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
-    @Test
-    void testFizzBuzzStringForF() {
+   
+    @Test 
+    void fizzBuzzTestForNull() {
+        String response = FizzBuzz.fizzBuzzString("");
+        assertEquals(null, response);
+    }
+
+    @Test 
+    void fizzBuzzTestForF() {
         String response = FizzBuzz.fizzBuzzString("f");
         assertEquals("Fizz", response);
     }
 
-    @Test
-    void testFizzBuzzStringForB() {
+    @Test 
+    void fizzBuzzTestForB() {
         String response = FizzBuzz.fizzBuzzString("b");
         assertEquals("Buzz", response);
     }
 
-    @Test
-    void testFizzBuzzStringForFAndB() {
+    @Test 
+    void fizzBuzzTestForFAndB() {
         String response = FizzBuzz.fizzBuzzString("fb");
         assertEquals("FizzBuzz", response);
     }
